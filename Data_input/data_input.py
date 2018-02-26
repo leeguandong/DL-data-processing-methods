1e726d24473be1e9a2411e3f31a89d5b92e4f454
Leeguandong <897158662@qq.com>
2018-02-26 13:51:53 +0800

    更新

--- Data_input/data_input.py
+++ Data_input/data_input.py
@@ -2,26 +2,49 @@ import numpy as np
 from keras.applications.imagenet_utils import decode_predictions
 from keras.preprocessing import image
 from keras.applications import *
+import glob
 
 import os
 
 # 忽略硬件加速的警告信息
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
-file_path = 'images/0a70f64352edfef4c82c22015f0e3a20.jpg'
+file_path = 'Data/'
+f_names = glob.glob(file_path + '*.jpg')
 
-img = image.load_img(file_path, target_size=(224, 224))
-x = image.img_to_array(img)
-x = np.expand_dims(x, axis=0)
+img = []
+# 把图片读取出来放到列表中
+for i in range(len(f_names)):
+    images = image.load_img(f_names[i], target_size=(224, 224))
+    x = image.img_to_array(images)
+    x = np.expand_dims(x, axis=0)
+    img.append(x)
+    print('loading no.%s image' % i)
+
+# 把图片数组联合在一起
+x = np.concatenate([x for x in img])
 
 model = ResNet50(weights='imagenet')
 y = model.predict(x)
-# print(np.argmax(y))
-print('Predicted:', decode_predictions(y, top=3)[0])
+print('Predicted:', decode_predictions(y, top=3))
+'''
+decode_predictions 返回的是一个列表，所有batch的
+# Returns
+        A list of lists of top class prediction tuples
+        `(class_name, class_description, score)`.
+        One list of tuples per sample in batch input.
+'''
+# [[('n02100735', 'English_setter', 0.62966996), ('n02101556', 'clumber', 0.30187815), ('n02101388', 'Brittany_spaniel', 0.023777956)],
+# [('n04589890', 'window_screen', 0.36741936), ('n03598930', 'jigsaw_puzzle', 0.16058789), ('n03000247', 'chain_mail', 0.064882509)],
+# [('n02099712', 'Labrador_retriever', 0.18316899), ('n02092339', 'Weimaraner', 0.17261373), ('n02107312', 'miniature_pinscher', 0.084056087)],
+# [('n02097298', 'Scotch_terrier', 0.71131712), ('n02096177', 'cairn', 0.16914253), ('n02098286', 'West_Highland_white_terrier', 0.064419992)],
+# [('n02108915', 'French_bulldog', 0.95837146), ('n02110958', 'pug', 0.036947753), ('n02108089', 'boxer', 0.0012087316)],
+# [('n02090721', 'Irish_wolfhound', 0.63058007), ('n02111129', 'Leonberg', 0.094695911), ('n02106382', 'Bouvier_des_Flandres', 0.086082667)],
+# [('n02085936', 'Maltese_dog', 0.70757532), ('n02113624', 'toy_poodle', 0.081604019), ('n02096437', 'Dandie_Dinmont', 0.037193742)],
+# [('n02088364', 'beagle', 0.41100392), ('n02092339', 'Weimaraner', 0.11080339), ('n02100236', 'German_short-haired_pointer', 0.11025359)],
+# [('n02102318', 'cocker_spaniel', 0.84692949), ('n02113799', 'standard_poodle', 0.14155339), ('n02102040', 'English_springer', 0.0078855371)]]
 
 '''
-解释下步骤，load_img是keras调用了pillow的Image函数，对指定file_path的图片进行提取，然后使用img_to_array把图片转换成
-为numpy数组，shape为(224, 224, 3)，而expand_dims的作用是把shape(224, 224, 3)转换成(0, 224, 224, 3)，为什么要这个
-expand_dims？因为模型本身要求输入尺寸是(None, 224, 224, 3)，这个None表示batch，意思是你要输入多少张图片模型是不知道的，
-所以就用None来表示，而当你输入图片的时候，shape必须跟模型输入的shape保持一致才能输入。
+concatenate的作用是把shape为(0, 224, 224, 3)的每张图片tensor，打包成shape为(batch, 224, 224, 3)的tensor，
+这样就能实现批量预测或批量训练了
 '''
